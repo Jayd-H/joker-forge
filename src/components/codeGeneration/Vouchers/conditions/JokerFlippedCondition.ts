40099a678850bfbf643bfbdf690b1fdd3b0bc50e
cavoshoki <cavoshoku@gmail.com>
2025-09-29 15:31:30 +0200

    Remove: removed a few unused conditions for vouchers

--- src/components/codeGeneration/Vouchers/conditions/JokerFlippedCondition.ts
+++
@@ -1,7 +0,0 @@
-
-export const generateJokerFlippedConditionCode = (): string | null => {
-    return `(function()
-        return G.jokers.highlighted[1].facing == "back" == true
-    end)()`
-};
-
--- src/components/codeGeneration/Vouchers/conditions/SystemCondition.ts
+++
@@ -1,9 +0,0 @@
-import type { Rule } from "../../../ruleBuilder/types";
-
-export const generateSystemConditionCode = (
-  rules: Rule[],
-): string | null => {
-  const condition = rules[0].conditionGroups[0].conditions[0];
-
-  return `love.system.getOS() == "${condition.params?.system || "Windows"}"`;
-};
