acc20d9e63167a297cd7956926fa8b23421238df
Butter Stutter <butterstutter1@gmail.com>
2025-10-23 13:05:34 +1100

    Feat: Began adding code gen files for conditions

---
+++ src/components/codeGeneration/conditionUtils.ts
@@ -0,0 +1,27 @@
+import { GameObjectData, JokerData } from "../data/BalatroUtils";
+import { Condition, Rule } from "../ruleBuilder";
+import { generatePokerHandConditionCode } from "./Conditions/pokerHandCondition";
+
+
+const generateSingleConditionCode = (
+  condition: Condition,
+  rule: Rule,
+  itemType: string,
+  joker?: JokerData,
+): string | null => {
+  const singleConditionRule = {
+    ...rule,
+    conditionGroups: [
+      {
+        ...rule.conditionGroups[0],
+        conditions: [condition],
+      },
+    ],
+  };
+
+  switch (condition.type) {
+    case "hand_type":
+        return generatePokerHandConditionCode([singleConditionRule], itemType, joker)
+  }
+  return null
+}
