7f222eb38fa16292c6c78ddc27b6f72ca3c82ca3
Butter Stutter <butterstutter1@gmail.com>
2025-10-25 19:21:02 +1100

    Feat: Began programming a system to update all outdated rule blocks

---
+++ src/components/generic/RuleBlockUpdater.ts
@@ -0,0 +1,110 @@
+import { ConsumableData, DeckData, EditionData, EnhancementData, JokerData, SealData, VoucherData } from "../data/BalatroUtils";
+import { Condition, Effect, Rule } from "../ruleBuilder";
+
+export const updateRuleBlocks = (
+  jokers: JokerData[],
+  consumables: ConsumableData[],
+  enhancements: EnhancementData[],
+  seals: SealData[],
+  editions: EditionData[],
+  vouchers: VoucherData[],
+  decks: DeckData[]
+) => {
+  const allData = [jokers, consumables, enhancements, seals, editions, vouchers, decks]
+
+  allData.forEach(objectType => {
+    objectType.forEach(item => {item.rules?.forEach(rule => {
+
+      rule.trigger = updateTrigger(rule)
+
+      rule.conditionGroups.forEach(group => {group.conditions.forEach(condition => {
+        condition = updateCondition(condition)
+      })})
+
+      rule.effects.forEach(effect => {
+        effect = updateEffect(effect)
+      })
+
+      rule.randomGroups.forEach(group => {group.effects.forEach(effect =>
+        effect = updateEffect(effect)
+      )})
+
+      rule.loops.forEach(group => {group.effects.forEach(effect =>
+        effect = updateEffect(effect)
+      )})
+
+    })
+  })
+  }) 
+}
+
+const updateTrigger = (
+  rule: Rule
+) => {
+  switch(rule.trigger) {
+    case "consumable_used": case "voucher_redeemed": case "deck_selected":
+      return "card_used"
+    case "card_held":
+      return "card_held_in_hand"
+    default:
+      return rule.trigger
+  }
+}
+
+const updateCondition = (
+  condition: Condition
+) => {
+
+  condition.id = updateConditionId(condition.id)
+  condition.params = updateConditionParams(condition.id, condition.params)
+
+  return condition
+}
+
+const updateConditionId = (
+  id: string
+) => {
+  switch(id) {
+    case "card_count":
+      return "hand_count"
+    default:
+      return id
+  }
+}
+
+const updateConditionParams = (
+  id: string, 
+  params: Record <string, unknown>
+): Record <string, unknown> => {
+  
+
+  return params
+}
+
+const updateEffect = (
+  effect: Effect
+) => {
+  effect.id = updateEffectId(effect.id)
+  effect.params = updateEffectParams(effect.id, effect.params)
+
+  return effect
+}
+
+const updateEffectId = (
+  id: string
+) => {
+  switch(id) {
+    
+    default:
+      return id
+  }
+}
+
+const updateEffectParams = (
+  id: string, 
+  params: Record <string, unknown>
+): Record <string, unknown> => {
+  
+
+  return params
+}
