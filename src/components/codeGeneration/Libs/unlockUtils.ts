b3b352df07e43ca10beb7abd3efae52bdce7a594
Butter Stutter <butterstutter1@gmail.com>
2025-11-06 13:47:14 +1100

    chore: fixed some imports

--- src/components/codeGeneration/Libs/unlockUtils.ts
+++ src/components/codeGeneration/Libs/unlockUtils.ts
@@ -1,5 +1,5 @@
 import {
-  JokerData,
+  VoucherData,
   EDITIONS,
   ENHANCEMENTS,
   POKER_HANDS,
@@ -7,13 +7,14 @@ import {
   SEALS,
   STICKERS,
   SUITS,
+  VOUCHERS,
+  JokerData,
   getRarityDropdownOptions,
 } from "../../data/BalatroUtils";
 
 export const unlockTriggerOptions = [
   { value: "hand_contents", label: "Cards Played" },
   { value: "modify_deck", label: "Cards In Deck" },
-  { value: "modify_jokers", label: "Jokers" },
   { value: "career_stat", label: "Career Stat" },
   { value: "discard_custom", label: "Discard Hand" },
   { value: "win", label: "Win a Run" },
@@ -39,6 +40,11 @@ export const sealOptions = [
   ...SEALS().map(({ key, label }) => ({ value: key, label })),
 ];
 
+export const voucherOptions = [
+  { value: "any", label: "Any Voucher" },
+  ...VOUCHERS().map(({ key, label }) => ({ value: key, label })),
+];
+
 export const editionOptions = [
   { value: "any", label: "Any Edition" },
   ...EDITIONS().map(({ key, label }) => ({ value: key, label })),
@@ -55,7 +61,6 @@ export const cardStatsOptions = [
   { value: "c_face_cards_played", label: "Face Cards Played" },
   { value: "c_cards_discarded", label: "Cards Discarded" },
   { value: "c_playing_cards_bought", label: "Playing Cards Bought" },
-  { value: "c_jokers_sold", label: "Jokers Sold" },
   { value: "c_cards_sold", label: "Cards Sold" },
 ];
 
@@ -86,10 +91,9 @@ export const pokerHandOptions = [
   ...POKER_HANDS.map(({ value, label }) => ({ value, label })),
 ];
 
-export const jokersOptions = [
-  { value: "max_jokers", label: "Max Jokers Owned" },
-  { value: "joker_count", label: "Jokers Owned" },
-  { value: "joker_slots", label: "Joker Slots" },
+export const vouchersOptions = [
+  { value: "voucher_count", label: "Vouchers Owned" },
+  { value: "voucher_slots", label: "Voucher Slots" },
 ];
 
 export const blindsOptions = [
@@ -105,7 +109,13 @@ export const resourcesOptions = [
   { value: "dollars", label: "Current Money" },
 ];
 
-export const unlockOptions = {
+export const jokersOptions = [
+  { value: "max_jokers", label: "Max Jokers Owned" },
+  { value: "joker_count", label: "Jokers Owned" },
+  { value: "joker_slots", label: "Joker Slots" },
+];
+
+export const jokerUnlockOptions = {
   hand_contents: {
     categories: [
       { value: "rank", label: "Rank", options: rankOptions },
@@ -228,6 +238,122 @@ export const unlockOptions = {
   },
 };
 
+
+export const vouchersunlockOptions = {
+  hand_contents: {
+    categories: [
+      { value: "rank", label: "Rank", options: rankOptions },
+      { value: "suit", label: "Suit", options: suitOptions },
+      {
+        value: "enhancement",
+        label: "Enhancement",
+        options: enhancementOptions,
+      },
+      { value: "seal", label: "Seal", options: sealOptions },
+      { value: "edition", label: "Edition", options: editionOptions },
+      { value: "poker_hand", label: "Poker Hand", options: pokerHandOptions },
+    ],
+  },
+  modify_deck: {
+    categories: [
+      { value: "rank", label: "Rank", options: rankOptions },
+      { value: "suit", label: "Suit", options: suitOptions },
+      {
+        value: "enhancement",
+        label: "Enhancement",
+        options: enhancementOptions,
+      },
+      { value: "seal", label: "Seal", options: sealOptions },
+      { value: "edition", label: "Edition", options: editionOptions },
+    ],
+  },
+  career_stat: {
+    categories: [
+      { value: "card_stats", label: "Cards", options: cardStatsOptions },
+      {
+        value: "consumable_stats",
+        label: "Consumables",
+        options: consumableStatsOptions,
+      },
+      {
+        value: "economy_stats",
+        label: "Economy",
+        options: economyStatsOptions,
+      },
+      { value: "run_stats", label: "Runs", options: runStatsOptions },
+    ],
+  },
+  discard_custom: {
+    categories: [
+      { value: "rank", label: "Rank", options: rankOptions },
+      { value: "suit", label: "Suit", options: suitOptions },
+      {
+        value: "enhancement",
+        label: "Enhancement",
+        options: enhancementOptions,
+      },
+      { value: "seal", label: "Seal", options: sealOptions },
+      { value: "edition", label: "Edition", options: editionOptions },
+      { value: "poker_hand", label: "Poker Hand", options: pokerHandOptions },
+    ],
+  },
+  // There are a lot of possible options for win run/round, we can add them in the future as needed
+  win: {
+    categories: [
+      { value: "vouchers", label: "Vouchers", options: vouchersOptions },
+      {
+        value: "played_hand",
+        label: "Hand has been Played",
+        options: pokerHandOptions,
+      },
+      {
+        value: "not_played_hand",
+        label: "Hand has not been Played",
+        options: pokerHandOptions,
+      },
+      {
+        value: "last_played_hand",
+        label: "Last Played Hand",
+        options: pokerHandOptions,
+      },
+      { value: "blinds", label: "Blinds", options: blindsOptions },
+      {
+        value: "resources",
+        label: "Player Resources",
+        options: resourcesOptions,
+      },
+    ],
+  },
+  round_win: {
+    categories: [
+      {
+        value: "played_hand",
+        label: "Hand has been Played",
+        options: pokerHandOptions,
+      },
+      {
+        value: "not_played_hand",
+        label: "Hand has not been Played",
+        options: pokerHandOptions,
+      },
+      {
+        value: "last_played_hand",
+        label: "Last Played Hand",
+        options: pokerHandOptions,
+      },
+      { value: "blinds", label: "Blinds", options: blindsOptions },
+      {
+        value: "resources",
+        label: "Player Resources",
+        options: resourcesOptions,
+      },
+    ],
+  },
+  chip_score: {
+    categories: [],
+  },
+};
+
 const propertyConditionGenerators: Record<
   string,
   (property: string, targetCard: string, comparison?: string) => string
@@ -243,6 +369,10 @@ const propertyConditionGenerators: Record<
     property === "any"
       ? `${target}.seal ~= nil`
       : `${target}.seal == "${property}"`,
+  voucher: (property, target) =>
+    property === "any"
+      ? `${target}.voucher ~= nil`
+      : `${target}.voucher == "${property}"`,
   edition: (property, target) =>
     property === "any"
       ? `${target}.edition ~= nil`
@@ -257,12 +387,12 @@ const propertyConditionGenerators: Record<
     `G.GAME.hands["${property}"].played ${comparison}`,
   not_played_hand: (property) => `G.GAME.hands["${property}"].played == 0`,
   last_played_hand: (property) => `G.GAME.last_hand_played == "${property}"`,
-  jokers: (property, _, comparison) => {
-    if (property === "joker_count")
-      return `#(G.jokers and G.jokers.cards or {}) ${comparison}`;
+  vouchers: (property, _, comparison) => {
+    if (property === "voucher_count")
+      return `#(G.vouchers and G.vouchers.cards or {}) ${comparison}`;
     if (property === "joker_slots")
-      return `(G.jokers and G.jokers.config.card_limit or 0) ${comparison}`;
-    if (property === "max_jokers") return `G.GAME.max_jokers ${comparison}`;
+      return `(G.vouchers and G.vouchers.config.card_limit or 0) ${comparison}`;
+    if (property === "max_vouchers") return `G.GAME.max_vouchers ${comparison}`;
     return "true";
   },
   blinds: (property, _, comparison) => {
@@ -287,9 +417,10 @@ const generatePropertyConditions = (
     return generator
       ? generator(property, targetCard ?? "", comparison)
       : "true";
-  });
+  }
+);
 
-export const generateUnlockFunction = (jokerData: JokerData) => {
+export const generateUnlockJokerFunction = (jokerData: JokerData) => {
   const unlockTrigger = jokerData.unlockTrigger;
   const unlockProperties = jokerData.unlockProperties;
   const unlockOperator = jokerData.unlockOperator;
@@ -318,7 +449,7 @@ export const generateUnlockFunction = (jokerData: JokerData) => {
 
   let unlockFunction = `,
   check_for_unlock = function(self,args)
-    if args.type == "${unlockTrigger}" then
+    if args.type == "${String(unlockTrigger)}" then
       local count = 0`;
 
   if (unlockTrigger === "chip_score") {
@@ -371,6 +502,107 @@ export const generateUnlockFunction = (jokerData: JokerData) => {
         unlockFunction += `
         for _, joker in ipairs(G.jokers.cards) do`;
         targetCard = `joker`;
+        break;
+    }
+
+    const conditions = generatePropertyConditions(
+      unlockProperties,
+      targetCard
+    )?.join(" and ");
+
+    unlockFunction += `
+    if ${conditions?.length ? conditions : "true"} then
+      count = count + 1
+    end
+    end
+      if count ${comparison} then
+        return true
+      end`;
+  }
+
+  unlockFunction += `
+  end
+  return false
+  end`;
+
+  return unlockFunction;
+};
+
+export const generateUnlockVoucherFunction = (voucherData: VoucherData) => {
+  const unlockTrigger = voucherData.unlockTrigger;
+  const unlockProperties = voucherData.unlockProperties;
+  const unlockOperator = voucherData.unlockOperator;
+  const unlockCount = voucherData.unlockCount ?? 1;
+
+  let comparison = "";
+  switch (unlockOperator) {
+    case "equals":
+      comparison = `== to_big(${unlockCount})`;
+      break;
+    case "greater_than":
+      comparison = `> to_big(${unlockCount})`;
+      break;
+    case "less_than":
+      comparison = `< to_big(${unlockCount})`;
+      break;
+    case "greater_equals":
+      comparison = `>= to_big(${unlockCount})`;
+      break;
+    case "less_equals":
+      comparison = `<= to_big(${unlockCount})`;
+      break;
+    default:
+      comparison = `== to_big(${unlockCount})`;
+  }
+
+  let unlockFunction = `,
+  check_for_unlock = function(self,args)
+    if args.type == "${String(unlockTrigger)}" then
+      local count = 0`;
+
+  if (unlockTrigger === "chip_score") {
+    unlockFunction += `\n return args.chips ${comparison}`;
+  } else if (unlockTrigger === "career_stat") {
+    unlockFunction += `\n return G.PROFILES[G.SETTINGS.profile].career_stats.${unlockProperties?.[0].property} ${comparison}`;
+  } else if (unlockTrigger === "win" || unlockTrigger === "round_win") {
+    const conditions = generatePropertyConditions(
+      unlockProperties,
+      "",
+      comparison
+    )?.join(" and ");
+
+    unlockFunction += `\n return ${conditions}`;
+  } else if (
+    unlockTrigger === "hand_contents" ||
+    unlockTrigger === "modify_deck" ||
+    unlockTrigger === "discard_custom"
+  ) {
+    let targetCard = "";
+
+    switch (unlockTrigger) {
+      case "discard_custom":
+      case "hand_contents":
+        unlockFunction += `
+        for i = 1, #args.cards do`;
+        targetCard = `args.cards[i]`;
+        break;
+
+      case "modify_deck":
+        unlockFunction += `
+        if deck_initialized == nil then
+          local deck_initialized = false
+        end
+
+        if not deck_initialized and #G.playing_cards >= 40 then
+          deck_initialized = true
+        end
+
+        if not deck_initialized then
+          return false
+        end
+
+        for _, card in ipairs(G.playing_cards or {}) do`;
+        targetCard = `card`;
         break;
     }
 
