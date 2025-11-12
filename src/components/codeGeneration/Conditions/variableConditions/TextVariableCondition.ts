ef8ca9881bbce11a4379cf4877ddf6d521de5536
Butter Stutter <butterstutter1@gmail.com>
2025-11-13 09:50:45 +1100

    fix: fixed text var condition not using key var correctly

--- src/components/codeGeneration/Conditions/variableConditions/TextVariableCondition.ts
+++ src/components/codeGeneration/Conditions/variableConditions/TextVariableCondition.ts
@@ -10,10 +10,31 @@ export const generateTextVariableConditionCode = (
   const keyVar = (condition.params?.key_variable as string) || "keyvar"
 
 switch (CheckType) {
-    case "custom_text":
-      return `card.ability.extra.${variableName} == '${customText}'`;
     case "key_var":
-      return `card.ability.extra.${variableName} == card.ability.extra.${keyVar}`;
+      return `(function()
+        local all_key_lists = {}
+        for _, pool in pairs(G.P_CENTER_POOLS) do
+            for _, item in pairs(pool) do
+                table.insert(all_key_lists, item)
+            end      
+        end
+        for _, current_card in pairs(all_key_lists) do
+            if current_card.key == card.ability.extra.${keyVar} then
+              if current_card.set == "Seal" then
+                if card.ability.extra.${variableName} == current_card.key then
+                  return true
+                end
+              else 
+                if card.ability.extra.${variableName} = current_card.name then
+                  return true
+                end
+              end
+              break
+            end
+        end
+        false
+      end)()`;
+    case "custom_text": 
     default:
       return `card.ability.extra.${variableName} == '${customText}'`;
   }
